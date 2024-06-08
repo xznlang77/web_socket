dd23346e75100c814d9102d97ae60ee0114b6312
xzn <xznlang77@gmail.com>
2024-06-08 11:35:43 +0300

    made class nested

--- src/main/java/xzn/Client.java
+++ src/main/java/xzn/Client.java
@@ -12,32 +12,32 @@ public class Client {
 
         client.connect();
     }
-}
 
-class SimpleWebSocketClient extends WebSocketClient {
+    static class SimpleWebSocketClient extends WebSocketClient {
 
-    public SimpleWebSocketClient(URI serverUri) {
-        super(serverUri);
+        public SimpleWebSocketClient(URI serverUri) {
+            super(serverUri);
+        }
+
+        @Override
+        public void onOpen(ServerHandshake handshakedata) {
+            System.out.println("Opened connection");
+        }
+
+        @Override
+        public void onMessage(String message) {
+            System.out.println("Received message from server: " + message);
+        }
+
+        @Override
+        public void onClose(int code, String reason, boolean remote) {
+            System.out.println("Closed connection");
+        }
+
+        @Override
+        public void onError(Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
-
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-        System.out.println("Opened connection");
-    }
-
-    @Override
-    public void onMessage(String message) {
-        System.out.println("Received message from server: " + message);
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        System.out.println("Closed connection");
-    }
-
-    @Override
-    public void onError(Exception ex) {
-        ex.printStackTrace();
-    }
-
 }
